c57921d455c9d778331ae6220bdd6c9ac7919898
ShailenS <sahshailendra.nitj@gmail.com>
2019-03-16 20:15:42 +0530

    first commit

---
+++ src/main/java/org/rest/controller/WelcomeController.java
@@ -0,0 +1,16 @@
+package org.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+
+    @GetMapping(value = "/")
+    public String greeting(){
+        return "hello";
+    }
+
+
+}
